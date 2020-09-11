60d48a252613aa2aae0861561436c56850b06f14
Sumon-miazi <sumonmiazi59@gmail.com>
2020-09-12 01:08:50 +0600

    #Added NotificationService #Added MapsActivity #Added google-services.json

--- app/src/main/java/com/example/bus_tracker/MainActivity.java
+++ app/src/main/java/com/example/bus_tracker/MainActivity.java
@@ -1,8 +1,9 @@
 package com.example.bus_tracker;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        startActivity(new Intent(this, MapsActivity.class));
+        // FirebaseMessaging.getInstance().subscribeToTopic("notification");
     }
 }
